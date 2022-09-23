dc10bd4896043465ee72b1d180a9c36d386a806c
favs tech <chinenyenwa3681@gmail.com>
2022-09-23 17:02:55 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,4 +1,7 @@
 #deploy a phyton web application 
-echo "hello world" = shell scripts
-python ('hello world') = python
-i am a senoir devop engineer
+echo "hello world" 
+# shell scripts
+python ('hello world') 
+#= python
+print('hello world')
+#i am a senoir devop engineer
